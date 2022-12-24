020a3645516b04a50a7bb9534ca4d778facd838a
PJH <carpediem.mementomori.1984@gmail.com>
2022-12-24 12:27:42 +0900

    Initial commit (basic setup)

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import mongoose from 'mongoose';
+
+import schema from '../graphql/schema/index';
+
+const app = express();
+const port = process.env.PORT;
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.40jgkye.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.Promise = global.Promise;
+mongoose.set('strictQuery', false);
+mongoose.connect(uri);
+// .then((res) => console.log(res.connection));
+
+app.use(`/graphql`, graphqlHTTP({ schema, graphiql: true }));
+
+app.get('/', (req, res) => {
+  res.json({
+    msg: '안녕',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`서버 실행!! 포트는? ${port}`);
+});
